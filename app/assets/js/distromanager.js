625663030cbf1a259d70baa3a1720e2e212ce8b0
Tealwen <lelayquentin3@gmail.com>
2024-02-02 20:04:28 +0100

    1.1.8

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'http://update.phynaria.fr/phynaria.json'
+exports.REMOTE_DISTRO_URL = 'http://ljkmrtj.cluster030.hosting.ovh.net/phynaria.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
